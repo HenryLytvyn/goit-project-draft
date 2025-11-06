9b3880957543bdb0ac096229e262f8f8d4746738
Henry <hennadii.lytvyn@gmail.com>
2025-11-06 16:23:33 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import css from './Home.module.css';
+
+export default function MainPage() {
+  return (
+    <main>
+      <p className={css.container}>Відкрийте світ подорожей з нами!</p>
+    </main>
+  );
+}

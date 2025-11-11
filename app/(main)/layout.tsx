7666f076fb93190f44d36ebc6a3f652107739a87
Henry <hennadii.lytvyn@gmail.com>
2025-11-11 01:44:18 +0000

    favicon, header links add

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,17 +1,23 @@
-import { ReactNode } from 'react';
+// import { ReactNode } from 'react';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import css from './Main.module.css';
 
 interface MainLayoutProps {
-  children: ReactNode;
+  children: React.ReactNode;
+  modal: React.ReactNode;
 }
 
-export default function MainLayout({ children }: MainLayoutProps) {
+export default function MainLayout({
+  children,
+  modal,
+}: Readonly<MainLayoutProps>) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className={css.main}>{children}</main>
       <Footer />
+      <div style={{ position: 'fixed', top: 0, left: 0 }}>{modal}</div>
     </>
   );
 }

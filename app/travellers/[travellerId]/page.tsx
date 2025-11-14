9219e0ce50797b2af7cdb7a09e8d1d44cf8fe74e
Ira <semikirina@ukr.net>
2025-11-14 10:53:35 +0100

    fixed issues

--- app/travellers/[travellerId]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useParams } from 'next/navigation';
-
-export default function TravelerPage() {
-  const params = useParams();
-  return <h2>TravelerId – {params.travellerId}</h2>;
-}

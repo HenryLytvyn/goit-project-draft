ca112eb15dae813f0f41cc4562f5b241782d1c35
Henry <hennadii.lytvyn@gmail.com>
2025-11-16 13:17:33 +0000

    remove bug with baseURL

--- components/AddStoryForm/api.ts
+++ components/AddStoryForm/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + 'api';
+// const baseURL = process.env.NEXT_PUBLIC_API_URL + 'api';
+const baseURL = 'http://localhost:3000/api';
 
 const nextServer = axios.create({
   baseURL,
